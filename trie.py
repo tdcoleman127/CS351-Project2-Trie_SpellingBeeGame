38347db8bdbcf552d41c279bef31ab39a45512ac
tdcoleman127 <figgeryfirehead@gmail.com>
2025-09-27 18:56:12 -0500

    Beginning to build Trie class, working on remove() and getFromFile()

--- trie.py
+++ trie.py
@@ -12,53 +12,115 @@ class Node:
         # What determines 
         self.isWord = False
 
-
 class Trie:
     """ A class for the Trie """
     def __init__ (self):
         self.root = Node("root")
-
+        self.trieDict = []
+        self.numWords = 0
         
-    def getFromFile():
+        
+    def getFromFile(self, filename:str) -> bool:
 
+        # for line in file:
+
+        for line in file:
+            self.trieDict.append(line)
+            
+        try:
+            fname = input("Enter filename: ").strip()
+        file = open(filename, 'r')
+        except EOFError:
+            print("No interactive input in the autograder environment.")
+            return
+        if not fname:
+            print("Empty filename.")
+            return
+
+        file.close()
+        return True
+        
         # functions for Step 1
-# def getFrequencyCounts(fname):
-#     # Intializing dictionary for frequency counts
-#     myDict = {}
+    # def getFrequencyCounts(fname):
+    #     # Intializing dictionary for frequency counts
+    #     myDict = {}
 
-#     # Open file
+    #     # Open file
 
-#     file = open(fname, 'r')
+    #     file = open(fname, 'r')
+        
+    #     # Read file for each character in each line
+    #     for line in file:
+    #         for char in line:
+    #             if char not in myDict:
+    #                 # if char not found, create key and give value 1
+    #                 myDict[char] = 1
+    #             else:
+    #                 # if found, increment frequency value
+    #                 myDict[char] += 1
+    #     file.close()
+    #     print("The dictionary of frequency counts for " + fname + " is: ")
+        
+    #     # Sorting dictionary values by value, ascending
+    #     newDict = dict(sorted(myDict.items(), key=lambda x:x[1]))
+    #     print(newDict)
+    #     return newDict
+        # pass
+
+    #Citing Professor Tryo's code from 9/25 lecture on Trie with static list of children
+    def insert(self, word:str) -> bool:
+        currNode = self.root
+        # Base case: Word already exists
+        if(currNode.isWord):
+            return False
+        
+        # For each character in the given word
+        for ch in word:
+            # If a character isn't a letter
+            if(ch.isalpha()):
+                return False
+            # Find the alphabetical index of the character
+            ind = ord(ch.lower()) - 97
+            # If it's not in the current 
+            if currNode.children([ind] == None):
+                currNode.children[ind] = Node(ch.lower())
+            # Move to the next character in the word
+            currNode = currNode.children[ind]
+            
+        currNode.isWord = True
+        self.numWords = self.numWords + 1
+        return True
+
+    # Search should find or not find a legit "word" no matter what
+    def search(self, word:str) -> bool:
+        currNode = self.root
+        for ch in word:
+            ind = ord(ch.lower()) - 97
+            if currNode.children[ind] == None:
+                return False
+            else:
+                currNode = currNode.children[ind]
+        return currNode.isWord
+
     
-#     # Read file for each character in each line
-#     for line in file:
-#         for char in line:
-#             if char not in myDict:
-#                 # if char not found, create key and give value 1
-#                 myDict[char] = 1
-#             else:
-#                 # if found, increment frequency value
-#                 myDict[char] += 1
-#     file.close()
-#     print("The dictionary of frequency counts for " + fname + " is: ")
-    
-#     # Sorting dictionary values by value, ascending
-#     newDict = dict(sorted(myDict.items(), key=lambda x:x[1]))
-#     print(newDict)
-#     return newDict
-        pass
-    
-    def insert():
-        pass
-    
-    def remove():
-        pass
+    def remove(self, word:str) -> bool:
+        currNode = self.root
+        # Go through the word, try to find it in the tree
+        for ch in word:
+            ind = ord(ch.lower()) - 97
+            currNode = currNode.children[ind]
+
+        if currNode.isWord == False:
+           return False
+        
+        currNode.isWord = False
+        return True
     
     def clear():
         pass
     
-    def wordCount():
-        pass
+    def wordCount(self) -> int:
+        return self.numWords
     
     def words():
         pass
